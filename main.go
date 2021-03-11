ba815034182129636dc9a041568c09590a7d91d0
Michał Czmiel <michalczmiel@outlook.com>
2021-03-11 20:08:20 +0100

    feat: add fork count and average commits per repo

--- main.go
+++ main.go
@@ -17,6 +17,12 @@ func contains(s []string, str string) bool {
 	return false
 }
 
+func fetchCommits(username string, repository string) ([]*github.RepositoryCommit, error) {
+	client := github.NewClient(nil)
+	commits, _, err := client.Repositories.ListCommits(context.Background(), username, repository, nil)
+	return commits, err
+}
+
 func fetchRepositories(username string) ([]*github.Repository, error) {
 	client := github.NewClient(nil)
 	repositories, _, err := client.Repositories.List(context.Background(), username, nil)
@@ -35,18 +41,36 @@ func main() {
 	}
 
 	languages := make([]string, len(repositories))
+	commitsCount := 0
+	forkCount := 0
 
 	for _, repository := range repositories {
 		language := *repository.Language
+
 		if !contains(languages, language) {
 			languages = append(languages, language)
 		}
+
+		if *repository.Fork {
+			forkCount += 1
+		} else {
+			commits, err := fetchCommits(username, *repository.Name)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				return
+			}
+			commitsCount += len(commits)
+		}
 	}
 
+	averageCommitsCountPerRepo := commitsCount / (len(repositories) - forkCount)
+
 	fmt.Printf("Number of repositories: %v \n", len(repositories))
+	fmt.Printf("Number of forks: %v \n", forkCount)
 	fmt.Print("Languages: ")
 	for _, language := range languages {
 		fmt.Printf("%v ", language)
 	}
 	fmt.Println()
+	fmt.Printf("Average number of commits in repository: %v \n", averageCommitsCountPerRepo)
 }
