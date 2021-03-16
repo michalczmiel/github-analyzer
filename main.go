563128bc17bd4abd68d545cef8f48cd5c1ced7a9
Michał Czmiel <michalczmiel@outlook.com>
2021-03-16 18:58:12 +0100

    feat: fetch portfolio url and account age

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/google/go-github/v33/github"
 )
@@ -39,15 +40,29 @@ func fetchRepositories(username string) ([]*github.Repository, error) {
 	return repositories, err
 }
 
+func fetchUser(username string) (*github.User, error) {
+	client := github.NewClient(nil)
+	user, _, err := client.Users.Get(context.Background(), username)
+	return user, err
+}
+
 type UserStats struct {
 	forkCount                  int
 	repositoriesCount          int
 	averageCommitsCountPerRepo int
 	uniqueLanguages            []string
+	joinedAt                   time.Time
+	portfolioUrl               string
+	accountAgeInYears          int
 }
 
 func fetchUserStats(username string) (*UserStats, error) {
 	const RepositoryCountToAnalyze = 6
+
+	user, err := fetchUser(username)
+	if err != nil {
+		return nil, err
+	}
 
 	repositories, err := fetchRepositories(username)
 	if err != nil {
@@ -89,11 +104,15 @@ func fetchUserStats(username string) (*UserStats, error) {
 
 	averageCommitsCountPerRepo := commitsCount / (repositoriesCount - forkCount)
 
+	accountAgeInYears := (time.Now().Year() - user.CreatedAt.Year())
+
 	userStats := UserStats{
 		averageCommitsCountPerRepo: averageCommitsCountPerRepo,
 		repositoriesCount:          repositoriesCount,
 		forkCount:                  forkCount,
 		uniqueLanguages:            uniqueLanguages,
+		portfolioUrl:               *user.Blog,
+		accountAgeInYears:          accountAgeInYears,
 	}
 
 	return &userStats, nil
@@ -109,7 +128,8 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
-
+	fmt.Printf("Portfolio url: %v \n", userStats.portfolioUrl)
+	fmt.Printf("Account age in years: %v \n", userStats.accountAgeInYears)
 	fmt.Printf("Number of repositories: %v \n", userStats.repositoriesCount)
 	fmt.Printf("Number of forks: %v \n", userStats.forkCount)
 	fmt.Print("Languages: ")
